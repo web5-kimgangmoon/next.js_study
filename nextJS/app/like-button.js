75e69272e2f32712607b3ce659af2f047b41a2ac
kimgangmoon <rkdans2111@naver.com>
2024-08-22 02:43:28 +0900

    feat:first_study

---
+++ nextJS/app/like-button.js
@@ -0,0 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
+export default function LikeButton() {
+  const [likes, setLikes] = useState(0);
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+  <button onClick={handleClick}>Like ({likes})</button>;
+}
