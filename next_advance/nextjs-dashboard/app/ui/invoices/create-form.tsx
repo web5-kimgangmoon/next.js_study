b59745253b8f2085e109397b0e4189428f0de09d
kimgangmoon <kimgangmoon>
2024-08-28 15:07:18 +0900

    feat:14 continue

--- next_advance/nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ next_advance/nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -1,16 +1,22 @@
-import { CustomerField } from '@/app/lib/definitions';
-import Link from 'next/link';
+"use client";
+
+import { useActionState } from "react";
+import { CustomerField } from "@/app/lib/definitions";
+import Link from "next/link";
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import { Button } from '@/app/ui/button';
+} from "@heroicons/react/24/outline";
+import { Button } from "@/app/ui/button";
+import { createInvoice, State } from "@/app/lib/actions";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState: State = { message: null, error: {} };
+  const [state, formAction] = useActionState(createInvoice, initialState);
   return (
-    <form>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
