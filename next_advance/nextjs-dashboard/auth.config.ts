0e56b3399bfafcfd37cf484bf74705dcc6cf808f
kimgangmoon <rkdans2111@naver.com>
2024-08-28 21:42:25 +0900

    feat:nextBaseStudyComplete

---
+++ next_advance/nextjs-dashboard/auth.config.ts
@@ -0,0 +1,21 @@
+import type { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  callback: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add proviedrs with an empty array for now
+} satisfies NextAuthConfig;
